1ddfd49bfd01235851adea686882fbb34911ed67
aarushi1609 <83212309+aarushi1609@users.noreply.github.com>
2022-05-26 17:41:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
+peter_pan = "";
+frozen = "";
+
 function setup()
 {
-    canvas = createCanvas(600, 500);
+    canvas = createCanvas(500, 400);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function preload()
+{
+    frozen = loadSound("frozen.mp3");
+    peter_pan = loadSound("peter_pan.mp3");
+}
+
+function draw()
+{
+    image(video, 0, 0, 500, 400);
 }
